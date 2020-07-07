bf1184615c72539e7a00c0f6ebb1b9137ae94c25
LorettaLuo <67842350+LorettaLuo@users.noreply.github.com>
2020-07-07 14:46:05 -0700

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -11,11 +11,41 @@ FROM employee
 WHERE salary< (SELECT MAX(salary) FROM employee);
 #delete the highest & choose the highest
 
+#181 emplpyees earning more than their managers 
+SELECT a.name 
+FROM employee a, employee b
+WHERE a.managerid = 'NULL' AND 
+      a.salary >b.salary AND a.managerid=b.id
+#???? a.managerid=b.id 代表啥没搞懂
+
+#182 duplicate emails
+SELECT email
+FROM person
+GROUP BY email
+HAVING count(*)>1;
+
+#183 customers who never order
+SELECT c.name AS customers
+FROM customers c
+LEFT JOIN orders o
+  ON c.id = o.customerid
+WHERE o.customerid IS NULL;
+
 #196 delete duplicate emails 
 DELETE p1
 FROM person p1, person p2
 WHERE p1.email = p2.email AND p1.id>P2.id
 #需要后续再看一遍
+
+#197 rising temperature
+SELECT a.id 
+FROM weather a, weather b
+WHERE a.recorddate-b.recorddate=1 AND
+      a.temperature>b.temperature
+#也可以写成a INNER JOIN b, WHERE DATEDIFF(a.data, b.date)=1 ADN...
+
+#577 employee bonus 
+
 
 #595 big countries
 SELECT name, population, area
@@ -23,12 +53,20 @@ FROM world
 WHERE area > 3000000 OR population >25000000;
 
 597 friend request2 1: overall acceptance rate 
-SELECT if
-FROM 
-WHERE r.sender_id = r.requester_id ADN 
-#需要后续再看一遍
+SELECT ifnull(round(count(DISTINCT requester_id, accepter_id)/count(DISTINCT sender_id, send_to_id),2),0) as accept rate
+FROM reuqest_accepted, friend_request
+#?????需要后续再看一遍
 
-
+610 triangle judgement 
+SELECT x,y,z
+CASE WHEN x+y>z OR 
+          x+z>y OR 
+          y+z>x
+     THEN 'Yes'
+     ELSE 'No'
+END AS 'triangle'
+FROM triangle 
+#CASE WHEN的例子，要记得重命名一个column用end as
 
 
 IFNULL(expr1,expr2)
