db7948c2307ae297e557d5b17cf3d2623851ac0d
LorettaLuo <67842350+LorettaLuo@users.noreply.github.com>
2020-08-04 15:27:39 -0700

    Update sqlzoo.sql

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -341,10 +341,52 @@ GROUP BY institution
 
 #Window function
 #1) Show the lastName, party and votes for the constituency 'S14000024' in 2017.
+SELECT lastName, party, votes
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY votes DESC
 
+#2) Show the party and RANK for constituency S14000024 in 2017. List the output by party
+SELECT party, votes,
+       RANK() OVER (ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY party
 
+#3) Use PARTITION to show the ranking of each party in S14000021 in each year. Include yr, party, votes and ranking (the party with the most votes is 1)
+SELECT yr, party, votes,
+      RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000021'
+ORDER BY party,yr
 
+#4) Edinburgh constituencies are numbered S14000021 to S14000026.
+# Use PARTITION BY constituency to show the ranking of each party in Edinburgh in 2017. Order your results so the winners are shown first, then ordered by constituency.
+SELECT constituency,party, votes,
+RANK() OVER (PARTITION BY constituency ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+   AND yr  = 2017
+ORDER BY posn, constituency, votes DESC
 
+#5) Show the parties that won for each Edinburgh constituency in 2017
+SELECT constituency, party 
+FROM (SELECT constituency,party, votes,
+      RANK() OVER (PARTITION BY constituency ORDER BY votes 
+      DESC) as posn
+      FROM ge
+      WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+      AND yr  = 2017) RK
+WHERE RK.posn=1
 
-
+#6) Show how many seats for each party in Scotland in 2017.
+SELECT a.party, count(1)
+FROM (SELECT constituency, party,
+      RANK() over(partition by constituency order by votes 
+      DESC) as posn
+      FROM ge
+      WHERE constituency like 'S%'
+      AND yr  = 2017) a 
+WHERE a.posn =1
+GROUP BY a.party
 
